c1a254e67e9abeaccd32c6bcf242e01921395cef
Zirok Guadron <zirokguadron11@gmail.com>
2019-05-05 22:00:41 -0600

    agregando lo del curso

---
+++ holamundo.js
@@ -0,0 +1,3 @@
+alert ("Hola mundo con JS!");
+document.write("hola mundo desde el fichero externo"); //el document hace referencia al entorno del documento en donde se incrustara  y write escribe eso.
+console.log("Mostrando algo en consola");// console es una funcion la cual funciona para interactuar con la consola del navegador directamente y log es la carga de ello.

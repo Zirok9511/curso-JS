70fcef292e6d77fc6482c9810d214e0fa527b84f
Zirok Guadron <zirokguadron11@gmail.com>
2019-05-06 22:13:46 -0600

    variables

---
+++ variables.js
@@ -0,0 +1,8 @@
+//variables en JS
+var pais = "El Salvador"; //la palabra reservada en js para declarar una variable es var
+var continente ="C.A.";
+var anio = 2019; // las int o numericos no necesitan comillas.
+var pais_y_anio = pais+' '+anio; //concatenando dos variables   
+
+console.log(pais, continente, anio);
+alert(pais_y_anio);

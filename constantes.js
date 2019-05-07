af61298af4fea3d6ee0dee979004f740bb080a77
Zirok Guadron <zirokguadron11@gmail.com>
2019-05-06 23:25:11 -0600

    conociendo las constantes en JS

---
+++ constantes.js
@@ -0,0 +1,9 @@
+// las constantes son una especie de variables pero las cuales no pueden cambiar.
+var letter ="Z";
+const letra="K";
+console.log(letter);
+console.log(letra);
+var letter = "B";
+//letra ="G"; //dara error en consola, quitar las plecas iniciales y observar el error.
+console.log(letter);
+console.log(letra);

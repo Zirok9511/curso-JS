75dfd4211ebde8b908eacfcffc694e47ad1f588d
Zirok Guadron <zirokguadron11@gmail.com>
2019-10-09 22:09:34 -0600

    otro repaso sencillo de JS

---
+++ main.js
@@ -0,0 +1,22 @@
+//console.log('Mensaje de consola');
+//console.warn('advertencia de consola');
+//console.error('error de consola');
+//let es una forma de declarar una variable solo para el bloque que se usara
+//const es una constante 
+/*var happiness = true;
+var DaysOfYear = 365;
+for (var MomentsOfLife=0; MomentsOfLife<=DaysOfYear; MomentsOfLife++){
+    if(happiness==true){
+    console.log('Live the moment vicho!')
+    }else{
+        console.log('Try again!!');
+    }
+    console.log('Live the life in the day'+MomentsOfLife+' of Year');
+}
+*/
+//tipo de variable
+//String, int, boolean, null, undefined
+const name='Zirok';
+console.log(typeof name); //muestra el tipo de variable
+//otra forma de concatenar
+console.log(`My name is ${name}`);
